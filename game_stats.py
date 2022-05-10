9b03b456393398c7e97bc67cebb4f55e723b5e2f
Igniit <93883534+Igniit@users.noreply.github.com>
2022-05-10 14:32:48 -0500

    Add files via upload

---
+++ game_stats.py
@@ -0,0 +1,27 @@
+import json
+
+
+class GameStats:
+    """Track statistics for Alien Invasion"""
+
+    def __init__(self,ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # Start the game in an active state.
+        self.game_active = False
+
+        # High score is stored in a json and read from.
+        filename = "highscores.json"
+        with open(filename) as highscore:
+            high_score = json.load(highscore)
+        self.high_score = int(high_score)
+
+        self.level = 1
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game"""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+
+
---
+++ player.py
@@ -0,0 +1,7 @@
+from datetime import timedelta
+
+
+class Player:
+    """A class to store player's name, session date and duration"""
+    def __init__(self):
+        self.player = input("Hello user. What's your name?")
